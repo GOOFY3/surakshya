e999e67a59b55961b6aba96a81624d67aa38540e
goofy3 <https://github.com/GOOFY3>
2018-04-01 01:59:32 +0530

    almost done

---
+++ routes/index.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var router = express.Router();
+const sgMail = require('@sendgrid/mail');
+
+<<<<<<< HEAD
+router.get('/',function(req,res,next){
+  res.redirect('/home');
+});
+
+router.get('/home', function(req, res, next) {
+ 	res.render('index');
+});
+
+router.get('/history',function(req,res,next){
+	res.render('history');
+});
+router.get('/services',function(req,res,next){
+  res.render('services');
+});
+router.get('/objectives', function(req, res, next) {
+ 	res.render('objectives');
+});
+router.get('/contact', function(req, res, next) {
+ 	res.render('contact');
+});
+router.get('/legality',function(req,res,next){
+	res.render('legality');
+});
+router.get('/gallery',function(req,res,next){
+	res.render('gallery');
+=======
+router.get('/', function(req, res, next) {
+ 	res.render('index');
+});
+
+router.get('/contact',function(req,res,next){
+	res.render('contact');
+>>>>>>> d42f764bfc397b13e3fcf23ff7d700014899d25c
+});
+
+router.post('/contact',function(req,res,next){
+    sgMail.setApiKey("");
+    const msg = {
+        to: "justblankb@gmail.com",
+        from: req.body.email,
+        subject: req.body.subject,
+        text: req.body.body,
+    };
+    sgMail.send(msg);
+    const msg2 = {
+        to: req.body.email,
+        from: "justblankb@gmail.com",
+        subject: req.body.subject,
+        text: req.body.body,
+    };
+    sgMail.send(msg2);
+    res.send("<html><link href='https://fonts.googleapis.com/icon?family=Material+Icons' rel='stylesheet'><script>alert('Please check your email..!!')</script><a href='http://localhost:3000/'><i class='material-icons'>arrow_back</i></a></html>");
+    });
+
+module.exports = router;
